838478de360d935acd8be6ec29b697ad5ea32990
Eshmeet Singh <eshmeetsingh2005@gmail.com>
2026-09-02 21:10:28 +0530

    added changed to refresh token

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -115,8 +115,8 @@ export const login = async (req, res) => {
 
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
